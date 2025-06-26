ed6abe1a7e613f1c07d53b7643945736c1ea3d48
subash shrestha <subashstha769@gmail.com>
2025-06-26 21:12:56 +0545

    Web Calculator: styled were changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <div className="container-fluid  w-[100vw]  h-[100vh] flex justify-center items-center overflow-auto">
+      <div className="container-fluid  w-[100vw]  h-[100vh] flex justify-center items-center overflow-auto lg:justify-end lg:pr-[200px]">
         <Calculator/>
       </div>
     </>
